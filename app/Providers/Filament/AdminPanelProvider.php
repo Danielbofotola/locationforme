308c9512b379f4e7033e1041af18974d908812e4
Danielbofotola <danielbofotola3@gmail.com>
2025-12-15 10:22:15 +0100

    premier commit

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => '#02869aff',
+                'primary' => '#323434ff',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
